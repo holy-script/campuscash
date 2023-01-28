71c4cfe5e8a01503b43627976a3524a4db462216
Palash Johri <palashjohricool@gmail.com>
2023-01-28 23:38:51 +0530

    vercel routing & deployment fix

---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Dashboard</h1>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "DashboardPage",
+});
+</script>
---
+++ src/pages/OnboardPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Onboard</h1>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "OnboardPage",
+});
+</script>
